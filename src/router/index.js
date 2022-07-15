d7794b0e659d62f798226c9388d9b64c37716057
Modenero <support@modenero.com>
2022-07-15 13:59:48 -0400

    Add Subnet page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import About from '../views/About.vue'
 import Home from '../views/Home.vue'
+import Subnet from '../views/Subnet.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/about',
+        component: About,
+    },
+    {
+        path: '/subnet',
+        component: Subnet,
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 })
 
 export default router
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <main>
+        <h1>About Ava's DAO</h1>
+
+    </main>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        //
+    }),
+    computed: {
+        //
+    },
+    methods: {
+        //
+    },
+    created: function () {
+        //
+    },
+    mounted: function () {
+        //
+    },
+}
+</script>
