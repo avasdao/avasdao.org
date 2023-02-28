97006758ec78ad2b83af279e6c5445a498f09dc5
Modenero <support@modenero.com>
2023-02-28 04:03:10 -0500

    Update meta tags.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    /* Application Settings */
+    app: {
+        /* Application Header */
+        head: {
+            title: `Ava's DAO — Best Crypto Experience (CX) For Your Money®`,
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { hid: 'description', name: 'description', content: `Ava Nakamoto showcases her life's work in a DAO that carries on where here father left off in 2010.` },
+                { name: 'format-detection', content: 'telephone=no' }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ],
+            script: [
+                { src: '/js/matomo.js' },
+            ],
+        },
+    },
+
+    /* Modules */
     modules: ['@nuxtjs/tailwindcss'],
 })
