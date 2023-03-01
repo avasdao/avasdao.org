841bea6ae6728fca621b2aa1e81d22e673a77431
Modenero <support@modenero.com>
2023-03-01 10:12:09 -0500

    Add ($AVAS) token page.

--- pages/Blank.vue
+++ pages/Blank.vue
@@ -6,8 +6,13 @@ const blank = ref(null)
 </script>
 
 <template>
-    <main>
-        <h1>Blank</h1>
+    <main class="w-11/12 lg:max-w-2xl px-3 py-5 bg-rose-100 border-4 border-rose-500 rounded-xl shadow-lg shadow-inner">
+        <h1 class="text-4xl font-medium">
+            Blank
+        </h1>
 
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
     </main>
 </template>
