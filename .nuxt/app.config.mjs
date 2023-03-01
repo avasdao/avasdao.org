c0d3d3894f679dd0606be849d3a5141f3c82dc9f
Modenero <support@modenero.com>
2023-03-01 10:11:45 -0500

    Remove `.nuxt`folder from repo.

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Workspace/avasdao/avasdao.org/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Workspace/avasdao/avasdao.org/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
